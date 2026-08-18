5b665e13740fe9e955c79a28cb7b21e6b1f31df8
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-08-18 22:01:39 +0300

    add notes pagination and filters

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -4,11 +4,31 @@ import createHttpError from 'http-errors';
 export const getAllNotes = async (req, res) => {
   const { page, perPage, tag, search } = req.query;
 
+  const skip = (page - 1) * perPage;
+
+  const filter = {};
+
+  if (tag) {
+    filter.tag = tag;
+  }
+
+  if (search) {
+    filter.$or = [
+      { title: { $regex: search, $options: 'i' } },
+      { content: { $regex: search, $options: 'i' } },
+    ];
+  }
+
+  const notes = await Note.find(filter).skip(skip).limit(perPage);
+
+  const totalNotes = await Note.countDocuments(filter);
+
   res.status(200).json({
+    notes,
     page,
     perPage,
-    tag,
-    search,
+    totalNotes,
+    totalPages: Math.ceil(totalNotes / perPage),
   });
 };
 
